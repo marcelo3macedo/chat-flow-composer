3349227ede1c3290c7b7decc1ce9283b26f8b99e
Marcelo Macedo <marcelo3macedo@gmail.com>
2025-07-18 15:29:07 -0300

    feat: adding background dynamic with zustand

--- src/components/Flow/FlowCanvas.tsx
+++ src/components/Flow/FlowCanvas.tsx
@@ -1,10 +1,13 @@
+import FlowBackground from "./FlowBackground";
 import FlowRenderer from "./FlowRenderer";
 import '@xyflow/react/dist/style.css';
 
 export default function FlowCanvas() {
   return (
     <div style={{ width: '100vw', height: '100vh' }}>
-      <FlowRenderer />
+      <FlowBackground>
+        <FlowRenderer />
+      </FlowBackground>
     </div>
   );
 }
