f857051e351e11bfb5f0d85955c580fffbbb09c0
Marcelo Macedo <marcelo3macedo@gmail.com>
2025-07-18 17:53:18 -0300

    feat: initial component

--- src/components/Plugins/index.ts
+++ src/components/Plugins/index.ts
@@ -1,7 +1,9 @@
+import { InitialNode } from "./Initial";
 import { MessageNode } from "./Message";
 import { RequestNode } from "./Request";
 
 export const nodeTypes = {
   messageNode: MessageNode,
   requestNode: RequestNode,
+  initialNode: InitialNode
 };
--- src/hooks/useFlowState.ts
+++ src/hooks/useFlowState.ts
@@ -2,10 +2,14 @@ import { useCallback, useState } from 'react';
 import { applyNodeChanges, applyEdgeChanges, addEdge, type Edge, type Node, type NodeChange, type EdgeChange, type Connection } from '@xyflow/react';
 
 const initialNodes = [
-  { id: 'n1', type: 'requestNode', position: { x: 0, y: 0 }, data: { label: 'Node 1' } },
-  { id: 'n2', type: 'messageNode', position: { x: 150, y: 0 }, data: { label: 'Node 2' } },
+  { id: 'n1', type: 'initialNode', position: { x: 0, y: 0 }, data: { label: 'Node 1' } },
+  { id: 'n2', type: 'requestNode', position: { x: 150, y: 0 }, data: { label: 'Node 2' } },
+  { id: 'n3', type: 'messageNode', position: { x: 300, y: 0 }, data: { label: 'Node 3' } },
 ];
-const initialEdges = [{ id: 'n1-n2', source: 'n1', target: 'n2' }];
+const initialEdges = [
+  { id: 'n1-n2', source: 'n1', target: 'n2' },
+  { id: 'n2-n3', source: 'n2', target: 'n3' }
+];
 
 export function useFlowState() {
   const [nodes, setNodes] = useState<Node[]>(initialNodes);
