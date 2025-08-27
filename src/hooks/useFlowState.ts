29c0e01465907003b0434a954b633ddf71869843
Marcelo Macedo <marcelo3macedo@gmail.com>
2025-08-27 16:08:42 -0300

    feat: register updates

--- src/hooks/useFlowState.ts
+++ src/hooks/useFlowState.ts
@@ -1,4 +1,4 @@
-import { type NodeChange, type EdgeChange, type Connection, type FinalConnectionState } from '@xyflow/react';
+import { type NodeChange, type EdgeChange, type Connection, type FinalConnectionState, applyNodeChanges, applyEdgeChanges, addEdge, type Edge } from '@xyflow/react';
 import { useCallback } from 'react';
 
 import useFlowContentStore from '@Composer/store/flow/content';
@@ -6,21 +6,27 @@ import useFlowContentStore from '@Composer/store/flow/content';
 import { useConnectState } from './useConnectState';
 
 export function useFlowState() {
-  const { workflow } = useFlowContentStore();
+  const { workflow, updateWorkflowField } = useFlowContentStore();
   const { connectionEnded } = useConnectState();
 
-  const onNodesChange = useCallback(
-    (_changes: NodeChange[]) => () => {},
-    [],
-  );
-  const onEdgesChange = useCallback(
-    (_changes: EdgeChange[]) => () => {},
-    [],
-  );
-  const onConnect = useCallback(
-    (_params: Connection) => () => {},
-    [],
-  );
+  const onNodesChange = (changes: NodeChange[]) => {
+    if (!workflow) return;
+    const newNodes = applyNodeChanges(changes, workflow.nodes);
+    updateWorkflowField('nodes', newNodes);
+  };
+
+  const onEdgesChange = (changes: EdgeChange[]) => {
+    if (!workflow) return;
+    const newEdges = applyEdgeChanges(changes, workflow.edges);
+    updateWorkflowField('edges', newEdges);
+  };
+
+  const onConnect = (connection: Connection) => {
+    if (!workflow) return;
+    const newEdges = addEdge(connection, workflow.edges);
+    updateWorkflowField('edges', newEdges);
+  };
+
   const onConnectEnd = useCallback((event: MouseEvent | TouchEvent, params: FinalConnectionState) => {
     connectionEnded(event, params);
   }, [connectionEnded]);
