a45d3014300085394244f5b2dd8fa10eef950f3b
Marcelo Macedo <marcelo3macedo@gmail.com>
2025-08-27 16:18:53 -0300

    feat: webhook node

--- src/components/Plugins/index.ts
+++ src/components/Plugins/index.ts
@@ -3,11 +3,13 @@ import { ManuallyNode } from "./Manually";
 import { MessageNode } from "./Message";
 import { RequestNode } from "./Request";
 import { VariablesNode } from "./Variables";
+import { WebhookNode } from "./Webhook";
 
 export const nodeTypes = {
   manually: ManuallyNode,
   message: MessageNode,
   request: RequestNode,
   initial: InitialNode,
-  variables: VariablesNode
+  variables: VariablesNode,
+  webhook: WebhookNode
 };
